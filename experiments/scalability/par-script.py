c021447128560717c1ec39e27b3ab4bf357394e7
grgill <grgill@sandia.gov>
2025-03-03 00:33:05 -0800

    debugging updates

--- experiments/scalability/par-script.py
+++ experiments/scalability/par-script.py
@@ -6,12 +6,12 @@ import argparse
 
 # Argument Parser
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("--build_dir", type=str, required=True, help="Path to the build directory")
-parser.add_argument("--results_dir", type=str, required=True, help="Path to directory where to save results")
-parser.add_argument("--data_dir", type=str, required=True, help="Path to the data directory")
-parser.add_argument("--test_name", type=str, required=True, help="Name of the test run")
-parser.add_argument("--ip", type=str, required=True, help="IP,ID of the ParDBClient")
-parser.add_argument("--filters", type=str, required=True, help="Text file with a list of filters on eachtheir own lines")
+parser.add_argument("--build_dir", type=str, required=False, help="Path to the build directory")
+parser.add_argument("--results_dir", type=str, required=False, help="Path to directory where to save results")
+parser.add_argument("--data_dir", type=str, required=False, help="Path to the data directory")
+parser.add_argument("--test_name", type=str, required=False, help="Name of the test run")
+parser.add_argument("--ip", type=str, required=False, help="IP,ID of the ParDBClient")
+parser.add_argument("--filters", type=str, required=False, help="Text file with a list of filters on eachtheir own lines")
 
 # Parse arguments
 args = parser.parse_args()
@@ -22,24 +22,30 @@ TEST_NAME = args.test_name
 IP = args.ip
 FILTER_FILE = args.filters
 
-sys.path.append(os.path.join(BUILD_DIR, 'pybind11'))
+# sys.path.append(os.path.join(BUILD_DIR, 'pybind11'))
+# sys.path.append("../../src/code/build/pybind11")
 import pando_api
-os.makedirs(RESULTS_DIR, exist_ok=True)
-
+# os.makedirs("results", exist_ok=True)
+print("Big ol' test")
 # Create client
-client = pando_api.ParDBClient(IP)
-
+client = pando_api.ParDBClient("10.100.4.88,0")
+print("client started")
 # Add filter directory
-client.add_filter_dir(os.path.join(BUILD_DIR, 'filters'))
 
+urrent_directory = os.getcwd()
+print(urrent_directory)
+client.add_filter_dir('../../src/code/build/filters')
+print("filters added")
 # Import data. This will need to be modified depending on how JSON gets implemented
-client.import_db(DATA_DIR)
-
+client.import_db("/cscratch/1024G")
+print("db imported")
 # Install all filters
-with open(FILTER_FILE, "r") as readable:
-    for line in readable:
-        client.install_filter(line.strip())  # Strip newline and carriage return
-
+# with open(FILTER_FILE, "r") as readable:
+#    for line in readable:
+#        client.install_filter(line.strip())  # Strip newline and carriage return
+client.install_filter("BTC_block_to_tx")
+client.install_filter("BTC_block_to_txtime")
+client.install_filter("BTC_tx_vals")
 # Begin test
 start = timer()
 client.process()  # Async
