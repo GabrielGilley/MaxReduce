55ad1a09a8912767d1dc3eaf7ffef4958b242cbd
gjacobu <gjacobu@sandia.gov>
2025-03-27 20:57:05 -0700

    small amount of cleanup

--- experiments/data/create-product-engagement-dataset.sh
+++ experiments/data/create-product-engagement-dataset.sh
@@ -13,6 +13,6 @@ PRODUCT_TABLE="product_table.csv"
 USER_TABLE="user_table.csv"
 
 # First, generate a list of product IDs. We'll just make them incrementing ints
-echo "$(shuf -r -i 1-$PRODUCT_SPEC_MAX -n $NUM_PRODUCT_IDS)"  | awk '{print NR "," $0}' > "$PRODUCT_TABLE"
+shuf -r -i 1-$PRODUCT_SPEC_MAX -n $NUM_PRODUCT_IDS | awk '{print NR "," $0}' > "$PRODUCT_TABLE"
 
 shuf -r -i 1-$NUM_PRODUCT_IDS -n $NUM_USERS | paste -d, - <(shuf -r -i 1-100 -n $NUM_USERS) | awk '{print NR "," $0}' > "$USER_TABLE"
